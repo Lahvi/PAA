3b24e84bfb35ded443b9a3c70d9c46749564672e
unknown <t945122@CZ00WL7012.gpcz.corp.local>
2013-10-22 16:45:31 +0200

    added generaly implementation of configuration, added JCOP libraries

--- src/model/KnapsackConfiguration.java
+++ src/model/KnapsackConfiguration.java
@@ -1,6 +1,48 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KnapsackConfiguration {
 	private int currentWeight;
 	private int currentPrice;
+	
+	private int capacity;
+	private List<KnapsackItem> items;
+	
+	public KnapsackConfiguration(int capacity){
+		this.capacity = capacity;
+		this.items = new ArrayList<KnapsackItem>();
+		currentPrice = 0;
+		currentWeight = 0;
+	}
+	
+	public boolean isPosibleAddItem(KnapsackItem item){
+		if((currentWeight + item.getWeight()) > capacity){
+			return false;
+		}
+		return true;
+	}
+	
+	public void addItem(KnapsackItem item){
+		items.add(item);
+		currentPrice += item.getPrice();
+		currentWeight += item.getWeight();
+	}
+
+	public int getCurrentWeight() {
+		return currentWeight;
+	}
+
+	public int getCurrentPrice() {
+		return currentPrice;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public List<KnapsackItem> getItems() {
+		return items;
+	}
 }
